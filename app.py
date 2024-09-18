ccc072a5a824512d5c3d26712da529e4498f2c60
pedrambigdelou <pedrambigdelou@gmail.com>
2024-09-17 23:15:21 -0400

    initial lines in app.py were added

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, request, render_template, redirect, flash, session
+from flask_debugtoolbar import DebugToolbarExtension
+from surveys import satisfaction_survey as survey
+
+# key names will use to store some things in the session;
+# put here as constants so we're guaranteed to be consistent in
+# our spelling of these
+RESPONSES_KEY = "responses"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = "never-tell!"
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+debug = DebugToolbarExtension(app)
