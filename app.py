310b1fc3574c5937b5a6aaf731092e943dd0dbfd
pedrambigdelou <pedrambigdelou@gmail.com>
2024-09-18 23:42:38 -0400

    the template for question and the function was tested and added

--- app.py
+++ app.py
@@ -26,3 +26,25 @@ def start_survey():
   session[RESPONSES_KEY] = []
 
   return redirect("/questions/0")
+
+@app.route("/questions/<int:qid>")
+def show_question(qid):
+  """Display current question."""
+  responses = session.get(RESPONSES_KEY)
+
+  if (responses is None):
+    # trying to access question page too soon
+    return redirect("/")
+
+  if (len(responses) == len(survey.questions)):
+    # They've answered all the questions! Thank them.
+    return redirect("/complete")
+
+  if (len(responses) != qid):
+    # Trying to access questions out of order.
+    flash(f"Invalid question id: {qid}.")
+    return redirect(f"/questions/{len(responses)}")
+
+  question = survey.questions[qid]
+  return render_template(
+    "question.html", question_num=qid, question=question)
